2fde543e172f6543ccf64da848a0a0d3fa14e61e
WyldnerPina <wyldner@gmail.com>
2022-09-28 02:20:19 -0300

    27/09/22 17:32

--- Aula04_Semaforos/Aula04_Semaforos_Exerc03/src/controller/Exerc03Controller.java
+++ Aula04_Semaforos/Aula04_Semaforos_Exerc03/src/controller/Exerc03Controller.java
@@ -4,7 +4,13 @@ import java.util.concurrent.Semaphore;
 
 public class Exerc03Controller extends Thread {
 	private int id;
-	private static int grid[] = new int[2];
+	private static int tamanhoGrid = 14;
+	private static int grid[] = new int[tamanhoGrid];
+	private static int aux[] = new int[tamanhoGrid];
+	private static int carro[] = new int[tamanhoGrid];
+	
+	private static String[] escuderia  = new String[tamanhoGrid];
+	
 	private Semaphore semaforo;
 	private Semaphore semaforoF;
 	private Semaphore semaforoML;
@@ -14,20 +20,19 @@ public class Exerc03Controller extends Thread {
 	private Semaphore semaforoRB;
 	private Semaphore semaforoS;
 
-	public Exerc03Controller(int id, Semaphore semaforoPista, Semaphore semaforoEquipes) {
+	public Exerc03Controller(int id, Semaphore sPista, Semaphore sF, Semaphore sML, Semaphore sM, Semaphore sW, Semaphore sL, Semaphore sRB, Semaphore sS) {
 		this.id = id;
-		this.semaforo = semaforoPista;
-		this.semaforoF = semaforoEquipes;
-		this.semaforoML = semaforoEquipes;
-		this.semaforoM = semaforoEquipes;
-		this.semaforoW = semaforoEquipes;
-		this.semaforoL = semaforoEquipes;
-		this.semaforoRB = semaforoEquipes;
-		this.semaforoS = semaforoEquipes;
+		this.semaforo = sPista;
+		this.semaforoF = sF;
+		this.semaforoML = sML;
+		this.semaforoM = sM;
+		this.semaforoW = sW;
+		this.semaforoL = sL;
+		this.semaforoRB = sRB;
+		this.semaforoS = sS;
 	}
 // =========================================================================================
 
-	
 	
 	
 // =========================================================================================
@@ -35,25 +40,26 @@ public class Exerc03Controller extends Thread {
 		equipes();
 	}
 // =========================================================================================
-
 	
 	
 	
 // =========================================================================================
 
 	private void equipes() {
-		String escuderia = "";
 		int voltas = 3;
-
+		
+		
 		if (id < 2) {
-			escuderia = "Ferrari";
+			escuderia[id] = "Ferrari";
+			carro[id] = id + 1;
+			
 			for (int i = 0; i < voltas; i++) {
-				System.out.println("Carro  " + (id + 1) + " == " + escuderia + " esperando liberação");
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
 				int tempo = 0;
 //			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
 				try {
 					semaforoF.acquire();
-					tempo = naPista(escuderia, tempo, i);
+					tempo = naPista(escuderia, tempo);
 
 					if (grid[id] == 0) {
 						grid[id] = tempo;
@@ -69,28 +75,191 @@ public class Exerc03Controller extends Thread {
 					semaforoF.release();
 				}
 //			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;
 			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------
+		} else if (id >= 2 && id < 4) {
+			escuderia[id] = "MacLaren";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoML.acquire();
+					tempo = naPista(escuderia, tempo);
 
-//		} else if (id >= 2 && id < 4) {
-//			escuderia = "MacLaren";
-//			System.out.println("Carro  " + (id+1) + " == " + escuderia);
-//		} else if (id >= 4 && id < 6) {
-//			escuderia = "Mercedes";
-//			System.out.println("Carro  " + (id+1) + " == " + escuderia);
-//		} else if (id >= 6 && id < 8) {
-//			escuderia = "Willians";
-//			System.out.println("Carro  " + (id+1) + " == " + escuderia);
-//		} else if (id >= 8 && id < 10) {
-//			escuderia = "Lotus";
-//			System.out.println("Carro  " + (id+1) + " == " + escuderia);
-//		} else if (id >= 10 && id < 12) {
-//			escuderia = "RedBull Racing";
-//			System.out.println("Carro  " + (id+1) + " == " + escuderia);
-//		} else {
-//			escuderia = "Sauber";
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoML.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;
+			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------			
+		} else if (id >= 4 && id < 6) {
+			escuderia[id] = "Mercedes";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoM.acquire();
+					tempo = naPista(escuderia, tempo);
+
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoM.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;
+			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------
+		} else if (id >= 6 && id < 8) {
+			escuderia[id] = "Willians";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoW.acquire();
+					tempo = naPista(escuderia, tempo);
+
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoW.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;
+			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------			
+		} else if (id >= 8 && id < 10) {
+			escuderia[id] = "Lotus";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoL.acquire();
+					tempo = naPista(escuderia, tempo);
+
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoL.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;
+			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------	
+		} else if (id >= 10 && id < 12) {
+			escuderia[id] = "RedBull Racing";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoRB.acquire();
+					tempo = naPista(escuderia, tempo);
+
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoRB.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;				
+			}
+// ------------------------------------------------------------------------------------	
+// ------------------------------------------------------------------------------------	
+		} else {
+			escuderia[id] = "Sauber";
+			carro[id] = id + 1;
+			
+			for (int i = 0; i < voltas; i++) {
+				System.out.println("Carro  " + (id + 1) + " == " + escuderia[id] + " esperando liberação");
+				int tempo = 0;
+//			--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
+				try {
+					semaforoS.acquire();
+					tempo = naPista(escuderia, tempo);
+
+					if (grid[id] == 0) {
+						grid[id] = tempo;
+					} else {
+						if (tempo < grid[id]) {
+							grid[id] = tempo;
+						}
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaforoS.release();
+				}
+//			----------------- FIM SEÇÃO CRÍTICA ----------------
+				aux[id] = i;				
+			}
 		}
 
-		grid(grid, escuderia);
+		grid();
 	}
 // =========================================================================================
 
@@ -98,18 +267,20 @@ public class Exerc03Controller extends Thread {
 	
 	
 // =========================================================================================
-	private int naPista(String escuderia, int tempo, int i) {
+	private int naPista(String[] escuderia, int tempo) {
 //		--------------- INÍCIO SEÇÃO CRÍTICA ---------------		
 		try {
 			semaforo.acquire();
-			System.out.println("Carro  " + (id + 1) + " da " + escuderia + " entrando na pista");
+			System.out.println("Carro  " + (id + 1) + " da " + escuderia[id] + " entrando na pista");
 
-			tempo = (int) ((Math.random() * 1001) + 1000);
+			tempo = (int) ((Math.random() * 2001) + 4000);// tempo na pista
 
 			try {
 				sleep(tempo);
-				System.err.println("===============" + escuderia + "===============\n" + "Carro  " + (id + 1) + " da "
-						+ escuderia + "\n" + "Volta  " + (i + 1) + " = " + tempo + "\n"
+				System.err.println("===============" + escuderia[id] + "===============\n"
+						+ "Carro  " + (id + 1) + " da "
+						+ escuderia[id] + "\n"
+						+ "Volta  " + (id + 1) + " = " + tempo + "\n"
 						+ "======================================");
 			} catch (InterruptedException e) {
 				e.printStackTrace();
@@ -120,6 +291,7 @@ public class Exerc03Controller extends Thread {
 			semaforo.release();
 		}
 //		----------------- FIM SEÇÃO CRÍTICA ----------------
+		
 		return tempo;
 	}
 // =========================================================================================
@@ -128,25 +300,70 @@ public class Exerc03Controller extends Thread {
 	
 	
 // =========================================================================================
-	private void grid(int grid[], String escuderia) {
+	private void grid() {
 		int cont = 0;
-		do {
-			for (int i = 0; i < grid.length; i++) {
-				if(grid[i] == 0) {
-				   cont += 1;	
-				}
-				System.out.println("Volta do piloto " + (i + 1) + " da " + escuderia + " = " + grid[i]);
+		int contAux = 0;
+		
+		while(cont < tamanhoGrid) {
+			if(aux[cont] == 2) {
+				contAux += 1;
 			}
-		}while(cont != 0);
-
+			cont++;
+		}
 		
-//		ajustarGrid();
 		
+		if(contAux == tamanhoGrid) {
+			for (int i = 0; i < tamanhoGrid; i++) {				
+				System.out.println("Melhor volta do piloto " + (i + 1) + " da " + escuderia[i] + " = " + grid[i]);
+			}
+			gridOficial();
+		}		
 	}
+	// =========================================================================================
+
 	
 	
-	private void ajustarGrid() {
+	
+	// =========================================================================================	
+	private void gridOficial() {
+		try {
+			sleep(500); // só um tempo pra não misturar com o anterior
+		} catch (InterruptedException e) {			
+			e.printStackTrace();
+		}
 		
 		
+		for (int i = 0; i < tamanhoGrid; i++) {		
+			for (int j = 0; j < tamanhoGrid; j++) {
+				if(grid[i] < grid[j]) {
+					int car = carro[i];
+					int aux = grid[i];
+					String auxi = escuderia[i];
+					carro[i] = carro[j];
+					grid[i] = grid[j];
+					escuderia[i] = escuderia[j];
+					carro[j] = car;
+					grid[j] = aux;
+					escuderia[j] = auxi;					
+				}				
+			}
+		}
+		
+		
+		for (int i = 0; i < tamanhoGrid; i++) {
+			if(i == 0) {
+				System.err.println("\n==================== Grid official F1 2022 ====================");
+			}
+			System.out.println((i + 1) + " colocado " + carro[i] + " " + escuderia[i] + " tempo: " + grid[i]);
+			if(i == (tamanhoGrid - 1)) {
+				try {
+					sleep(500); // só um tempo pra não misturar com o anterior
+					System.err.println("===============================================================");
+				} catch (InterruptedException e) {			
+					e.printStackTrace();
+				}
+				
+			}
+		}	
 	}
 }
--- Aula04_Semaforos/Aula04_Semaforos_Exerc03/src/view/Main.java
+++ Aula04_Semaforos/Aula04_Semaforos_Exerc03/src/view/Main.java
@@ -6,12 +6,18 @@ import controller.Exerc03Controller;
 
 public class Main {
 	public static void main(String[] args) {		
-		Semaphore semaforoPista = new Semaphore(5);
-		Semaphore semaforoEquipes = new Semaphore(1);
+		Semaphore sPista = new Semaphore(5);
+		Semaphore sF = new Semaphore(1);
+		Semaphore sML = new Semaphore(1);
+		Semaphore sM = new Semaphore(1);
+		Semaphore sW = new Semaphore(1);
+		Semaphore sL = new Semaphore(1);
+		Semaphore sRB = new Semaphore(1);
+		Semaphore sS = new Semaphore(1);
 
 // =============================================================================================		
-		for (int id = 0; id < 2; id++) {
-			Thread thread = new Exerc03Controller(id, semaforoPista, semaforoEquipes);
+		for (int id = 0; id < 14; id++) {
+			Thread thread = new Exerc03Controller(id, sPista, sF, sML, sM, sW, sL, sRB, sS);
 			thread.start();
 		}
 	}
